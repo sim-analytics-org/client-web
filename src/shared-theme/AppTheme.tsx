075c9c7a7f7027645d22a7d6691560114e12a423
Aidan Melvin <amelvin@umd.edu>
2025-06-10 17:12:37 -0400

    updated theme

--- src/shared-theme/AppTheme.tsx
+++ src/shared-theme/AppTheme.tsx
@@ -31,7 +31,7 @@ export default function AppTheme(props: AppThemeProps) {
         // colorSchemes, // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
         colorSchemes: {
           light: true,
-          dark: true,
+          dark: false,
         },
         typography,
         shadows,
